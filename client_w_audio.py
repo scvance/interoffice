e6331744e282819312184108f89f9032f2c06ef1
Sam Vance <samvance243@gmail.com>
2024-03-15 21:33:17 -0600

    AUDIO STREAMING WORKS CORRECTLY!!!

--- client_w_audio.py
+++ client_w_audio.py
@@ -1,5 +1,5 @@
 import cv2
-import sounddevice as sd
+import alsaaudio as aa
 import socketio
 import numpy as np
 import threading
@@ -9,20 +9,11 @@ import zlib
 SERVER_IP = '192.168.2.153'  # get your server's IP and put it here
 SOCKETIO_URL = f'ws://{SERVER_IP}:1234/'
 
-FORMAT = 'int16'  # Sounddevice uses string format for specifying data types
+FORMAT = aa.PCM_FORMAT_S16_LE  # Use 16-bit signed little-endian format
 CHANNELS = 1
 RATE = 8000
+CHUNK = 1024
 sio = socketio.Client()
-
-compressor = zlib.compressobj(level=6, strategy=zlib.Z_DEFAULT_STRATEGY)
-# Compress and decompress functions for video frames
-def compress_video(frame):
-    return zlib.compress(frame) # compressor.compress(encoded_frame.tobytes()) # + compressor.flush()
-
-
-def decompress_video(compressed_frame):
-    return cv2.imdecode(np.frombuffer(zlib.decompress(compressed_frame), dtype=np.uint8), cv2.IMREAD_COLOR)
-
 
 # Compress and decompress functions for audio frames
 def compress_audio(audio_data):
@@ -32,6 +23,8 @@ def compress_audio(audio_data):
 def decompress_audio(compressed_audio):
     return zlib.decompress(compressed_audio)
 
+def compress_video(frame):
+    return zlib.compress(frame)
 
 @sio.event
 def connect():
@@ -45,39 +38,43 @@ def disconnect():
 
 @sio.event
 def message(frame_data=None):
-    try:
-        if frame_data is not None:
-            if frame_data['audio'] is not None:
-                # audio_data = decompress_audio(frame_data['audio'])
-                audio_data = np.frombuffer(frame_data['audio'], dtype=np.int16)
-                sd.play(audio_data, samplerate=RATE)
-                sd.wait()
-            elif frame_data['video'] is not None:
-                video_frame = np.frombuffer(frame_data['video'], dtype=np.uint8) # decompress_video(frame_data['video'])
-                video_frame = cv2.imdecode(video_frame, cv2.IMREAD_COLOR)
-                cv2.imshow('Video Stream', video_frame)
+    if frame_data is not None:
+        if frame_data['audio'] is not None:
+            audio_data = np.frombuffer(frame_data['audio'], dtype=np.int16)
+            # audio_data_bytes = audio_data.tobytes()
+            audio_out = aa.PCM(aa.PCM_PLAYBACK, aa.PCM_NORMAL, channels=CHANNELS, rate=RATE, format=FORMAT, periodsize=CHUNK)
+            try:
+                for chunk in audio_data:
+                    audio_out.write(chunk)
+                    # time.sleep(0.1)  # Adjust timing to synchronize playback
+            except aa.ALSAAudioError as e:
+                print("ALSA Audio Error:", e)
+            finally:
+                audio_out.close()
+        elif frame_data['video'] is not None:
+            video_frame = np.frombuffer(frame_data['video'], dtype=np.uint8)
+            video_frame = cv2.imdecode(video_frame, cv2.IMREAD_COLOR)
+            cv2.imshow('Video Stream', video_frame)
 
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    sio.disconnect()
-                    cv2.destroyAllWindows()
-
-    except Exception as e:
-        pass
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                sio.disconnect()
+                cv2.destroyAllWindows()
 
 
 def send_audio():
+    audio_in = aa.PCM(aa.PCM_CAPTURE, aa.PCM_NORMAL, channels=CHANNELS, rate=RATE, format=FORMAT, periodsize=CHUNK)
     while True:
-        audio = sd.rec(int(RATE), samplerate=RATE, channels=CHANNELS, dtype=FORMAT)
-        sd.wait()  # Wait until recording is finished
-        audio_compressed = compress_audio(audio.tobytes())
+        audio_chunks = []
+        for i in range(0, int(RATE / CHUNK)):
+            length, data = audio_in.read()
+            audio_chunks.append(data)
+        audio_compressed = compress_audio(np.array(audio_chunks).tobytes())
         sio.emit('send_audio', {'audio': audio_compressed, 'room': 1})
-        time.sleep(.05)
+        time.sleep(.01)
 
 
 def send_frames():
     cap = cv2.VideoCapture(0)
-    # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
-    # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 640)
     while True:
         success, frame = cap.read()
         if not success:
@@ -86,8 +83,7 @@ def send_frames():
         encoded_frame = encoded_frame.tobytes()
         frame_compressed = compress_video(encoded_frame)
         sio.emit('send_frame', {'video': frame_compressed, 'room': 1})
-        # Adjust frame rate here according to your requirements
-        time.sleep(0.05)
+        time.sleep(.05)
 
     cap.release()
 
@@ -99,12 +95,11 @@ if __name__ == '__main__':
         # Start sending frames in a separate thread
         thread = threading.Thread(target=send_frames)
         audio_thread = threading.Thread(target=send_audio)
-        thread.start()
         audio_thread.start()
+        thread.start()
         # Keep the main thread alive to handle SocketIO events
         sio.wait()
     except KeyboardInterrupt:
         pass
-
-    # Wait for the send_frames thread to finish before exiting
     thread.join()
+    audio_thread.join()
--- test_audio.py
+++ test_audio.py
@@ -1,22 +1,37 @@
-import sounddevice as sd
+import alsaaudio
+import zlib
 import numpy as np
 
-CHUNK = 4096
-FORMAT = 'int16'  # Sounddevice uses string format for specifying data types
+CHUNK = 1024
+FORMAT = alsaaudio.PCM_FORMAT_S16_LE
 CHANNELS = 1
 RATE = 8000
 
 # Record audio
-print("Recording audio...")
-audio_data = sd.rec(int(RATE * 10), samplerate=RATE, channels=CHANNELS, dtype=FORMAT)
-sd.wait()  # Wait until recording is finished
-print("Audio recorded")
+audio_in = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NORMAL, channels=CHANNELS, rate=RATE, format=FORMAT, periodsize=CHUNK)
+# audio_in.setchannels(CHANNELS)
+# audio_in.setrate(RATE)
+# audio_in.setformat(FORMAT)
+# audio_in.setperiodsize(CHUNK)
 
-audio_data = audio_data.tobytes()
-audio_data = np.frombuffer(audio_data, dtype=np.int16)
+audio_chunks = []
+for i in range(0, int(RATE / CHUNK)):
+    length, data = audio_in.read()
+    audio_chunks.append(data)
 
-# Play the recorded audio
-print("Playing audio...")
-sd.play(audio_data, samplerate=RATE)
-sd.wait()  # Wait until playback is finished
-print("Audio playback finished")
+audio_chunks = zlib.compress(np.array(audio_chunks).tobytes())
+audio_chunks = zlib.decompress(audio_chunks)
+audio_chunks = np.frombuffer(audio_chunks, dtype=np.int16)
+
+# Play audio
+audio_out = alsaaudio.PCM(alsaaudio.PCM_PLAYBACK, alsaaudio.PCM_NORMAL, channels=CHANNELS, rate=RATE, format=FORMAT, periodsize=CHUNK)
+# audio_out.setchannels(CHANNELS)
+# audio_out.setrate(RATE)
+# audio_out.setformat(FORMAT)
+# audio_out.setperiodsize(CHUNK)
+
+for chunk in audio_chunks:
+    audio_out.write(chunk)
+
+print("Audio recorded and played")
+
