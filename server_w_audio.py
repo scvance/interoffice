3a898174bc8005bb6fcd619c224d843472148901
Sam Vance <samvance243@gmail.com>
2024-03-15 19:04:45 -0600

    added server_w_audio

--- server_w_audio.py
+++ server_w_audio.py
@@ -4,12 +4,16 @@ from flask import Flask, render_template
 import flask
 from flask_socketio import SocketIO, emit
 from flask_socketio import send, join_room, leave_room
-import threading
+import cv2
+import numpy as np
+import zlib
+
+
+compressor = zlib.compressobj(level=6, strategy=zlib.Z_DEFAULT_STRATEGY)
 app = Flask(__name__)
 socketio = SocketIO(app)
 clients = {}
 clients_room = {}
-lock = threading.Lock()
 @socketio.on('connect')
 def handle_connect():
     client_id = flask.request.sid
@@ -23,22 +27,30 @@ def handle_disconnect():
         del clients[client_id]
     print(f'Client {client_id} disconnected')
 
-@socketio.on('send_frame')
-def handle_send_frame(frame_request):
-    frame_data = frame_request['frame']
+@socketio.on('send_audio')
+def handle_send_audio(frame_request):
+    audio_frame = frame_request['audio']
     room_number = frame_request['room']
     client_id = flask.request.sid
+    join_room(room_number)
+    audio_frame = zlib.decompress(audio_frame)
 
-    # with lock:
+    if audio_frame is not None:
+        socketio.send({'video': None, 'audio': audio_frame}, room=room_number, skip_sid=client_id)
+
+@socketio.on('send_frame')
+def handle_send_frame(frame_request):
+    video_frame = frame_request['video']
+    room_number = frame_request['room']
+    client_id = flask.request.sid
+    video_frame = zlib.decompress(video_frame)
 
     # Join the client to the specified room
     join_room(room_number)
 
     # Emit this data to all clients in the same room except the sender
-    if frame_data is not None:
-        socketio.send({'frame_data': frame_data}, room=room_number, skip_sid=client_id)
-
-        # leave_room(room_number)
+    if video_frame is not None:
+        socketio.send({'video': video_frame, 'audio': None}, room=room_number, skip_sid=client_id)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=1234, debug=True, allow_unsafe_werkzeug=True)
