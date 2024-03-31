ce20ac6aa62e88a479a339481fe7a60de1538e88
Evan Smith <72663440+EvanSmith93@users.noreply.github.com>
2024-03-30 18:52:19 -0600

    Remove client and server files without audio

--- server.py
+++ server.py
@@ -1,44 +1,71 @@
 # server.py
 
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 import flask
 from flask_socketio import SocketIO, emit
 from flask_socketio import send, join_room, leave_room
-import threading
+import cv2
+import numpy as np
+import zlib
+
+
+compressor = zlib.compressobj(level=6, strategy=zlib.Z_DEFAULT_STRATEGY)
 app = Flask(__name__)
 socketio = SocketIO(app)
-clients = {}
+
+rooms = ["Evan's office", "Sam's office", "Meeting room", "Break room"]
 clients_room = {}
-lock = threading.Lock()
+
+# moves the client to a new room (if necessary)
+def change_rooms(client_id, new_room):
+    old_room = clients_room[client_id]
+    if old_room != new_room:
+        if old_room != None:
+            leave_room(f'room-{old_room}')
+        clients_room[client_id] = new_room
+        join_room(f'room-{new_room}')
+
 @socketio.on('connect')
 def handle_connect():
     client_id = flask.request.sid
-    clients[client_id] = None
+    clients_room[client_id] = None
     print(f'Client {client_id} connected')
 
 @socketio.on('disconnect')
 def handle_disconnect():
     client_id = flask.request.sid
-    if client_id in clients:
-        del clients[client_id]
+    if client_id in clients_room:
+        del clients_room[client_id]
     print(f'Client {client_id} disconnected')
+
+@socketio.on('send_audio')
+def handle_send_audio(frame_request):
+    audio_frame = frame_request['audio']
+    room_number = frame_request['room']
+    client_id = flask.request.sid
+    audio_frame = zlib.decompress(audio_frame)
+
+    change_rooms(client_id, room_number)
+
+    if audio_frame is not None:
+        socketio.send({'video': None, 'audio': audio_frame}, room=(f'room-{room_number}'), skip_sid=client_id)
 
 @socketio.on('send_frame')
 def handle_send_frame(frame_request):
-    frame_data = frame_request['frame']
+    video_frame = frame_request['video']
     room_number = frame_request['room']
     client_id = flask.request.sid
+    video_frame = zlib.decompress(video_frame)
 
-    # with lock:
-
-    # Join the client to the specified room
-    join_room(room_number)
+    change_rooms(client_id, room_number)
 
     # Emit this data to all clients in the same room except the sender
-    if frame_data is not None:
-        socketio.send({'frame_data': frame_data}, room=room_number, skip_sid=client_id)
+    if video_frame is not None:
+        socketio.send({'video': video_frame, 'audio': None}, room=(f'room-{room_number}'), skip_sid=client_id)
 
-        # leave_room(room_number)
+@app.route('/rooms')
+def get_rooms():
+    return jsonify(rooms)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=1234, debug=True, allow_unsafe_werkzeug=True)
