f64531813ab2da1fc94cdc8a82c13df5459f6f55
Sam Vance <samvance243@gmail.com>
2024-04-01 22:17:39 -0600

    testing 8fps

--- client.py
+++ client.py
@@ -133,7 +133,7 @@ def send_audio():
 def send_frames():
     global video_room
     cap = cv2.VideoCapture(0)
-    framerate = 10
+    framerate = 15
     while True:
         start_time = time.time()
         success, frame = cap.read()
