60292ff0611d44b2cebc2c5d1aaa11a0325c00f3
Sam Vance <samvance243@gmail.com>
2024-04-10 10:37:54 -0600

    modified setup script

--- setup_start.sh
+++ setup_start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 mkdir -p ~/.virtualenvs
-python3 -m venv ~/.virtualenvs/interoffice_venv
-source ~/.virtualenvs/interoffice_venv/bin/activate
-pip3 install -r requirements.txt
+venv_location="$HOME/.virtualenvs/interoffice_venv"
+if [ -d "$venv_location" ]; then
+	echo "Already set up, skipping setup steps..."
+	source ~/.virtualenvs/interoffice_venv/bin/activate
+else
+	python3 -m venv ~/.virtualenvs/interoffice_venv
+	source ~/.virtualenvs/interoffice_venv/bin/activate
+	pip3 install -r requirements.txt
+fi
 python3 server.py
