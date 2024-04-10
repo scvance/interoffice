3fdd0a0895c9b7b76fe8dbd32577c53133242ddc
Sam Vance <samvance243@gmail.com>
2024-04-10 09:42:17 -0600

    added a slight delay

--- client.py
+++ client.py
@@ -153,7 +153,7 @@ def send_frames():
         except Exception as e:
             print("Error: ", e)
         video_room_lock.release()
-    time.sleep(.01)
+    time.sleep(.05)
     # while True:
     #     frame_chunk = []
     #     while len(frame_chunk) < 3:
