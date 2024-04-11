d0d1e6fc59db38718a95408267e9ed97f5f2185b
Evan Smith <72663440+EvanSmith93@users.noreply.github.com>
2024-04-10 18:26:06 -0600

    Delete old webrtc files

--- webrtc/webrtc_server.py
+++ webrtc/webrtc_server.py
@@ -1,62 +1,87 @@
 # server.py
-import aiohttp
-import asyncio
-from aiohttp import web
-from aiohttp.web_ws import WebSocketResponse
-from aiohttp import WSMsgType
-from aiortc import RTCPeerConnection, RTCSessionDescription
-from aiortc.contrib.media import MediaStreamTrack
-import numpy as np
-import cv2
-import pyaudio
-import audioop
-from webrtc.common import VideoStreamTrack, AudioStreamTrack
 
-routes = web.RouteTableDef()
+from flask import Flask, jsonify, render_template
+import flask
+from flask_socketio import SocketIO, emit
+from flask_socketio import send, join_room, leave_room
+import zlib
+import threading
 
-connected_clients = set()
-video_track = None
-audio_track = None
+dictionary_lock = threading.Lock()
+
+compressor = zlib.compressobj(level=6, strategy=zlib.Z_DEFAULT_STRATEGY)
+app = Flask(__name__)
+socketio = SocketIO(app, async_mode='threading')
+
+rooms = ["Evan's office", "Sam's office", "Meeting room", "Break room"]
+clients_room = {}
+clients_old_room = {}
+
+# moves the client to a new room (if necessary)
+def change_rooms(client_id, new_room):
+    global clients_room
+    global clients_old_room
+    old_room = clients_old_room[client_id] if client_id in clients_old_room.keys() else None
+    if new_room not in clients_room.keys():
+        dictionary_lock.acquire()
+        clients_room[new_room] = []
+        dictionary_lock.release()
+    if old_room is None:
+        if new_room in clients_room.keys():
+            dictionary_lock.acquire()
+            clients_room[new_room].append(client_id)
+            dictionary_lock.release()
+        join_room(f'room-{new_room}')
+        dictionary_lock.acquire()
+        clients_old_room[client_id] = new_room
+        dictionary_lock.release()
+    elif client_id not in clients_room[new_room]:
+        leave_room(f'room-{old_room}')
+        dictionary_lock.acquire()
+        clients_room[old_room].remove(client_id)
+        clients_room[new_room].append(client_id)
+        join_room(f'room-{new_room}')
+        clients_old_room[client_id] = new_room
+        dictionary_lock.release()
+    dictionary_lock.acquire()
+    num_clients = len(clients_room[new_room])
+    client_index = clients_room[new_room].index(client_id)
+    dictionary_lock.release()
+
+    return num_clients, client_index
 
 
-@routes.get('/')
-async def index(request):
-    return web.Response(text="Hello, World!")
+@socketio.on('connect')
+def handle_connect():
+    client_id = flask.request.sid
+    print(f'Client {client_id} connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    client_id = flask.request.sid
+    print(f'Client {client_id} disconnected')
+
+@socketio.on('offer')
+def handle_rooms(request):
+    client_id = flask.request.sid
+    room_id = request['room_id']
+    offer = request['offer']
+    change_rooms(client_id, room_id)
+    socketio.send({'offerer_id': client_id, 'offer': offer, 'answerer_id': None, 'answer': None}, room=room_id, skip_sid=client_id)
+
+@socketio.on('answer')
+def handle_answer(request):
+    client_id = flask.request.sid
+    answer = request['answer']
+    receiver_id = request['receiver_id']
+    room_id = request['room_id']
+    print(receiver_id)
+    socketio.send({'answerer_id': client_id, 'answer': answer, "oferer_id": None, "offer": None}, room=room_id, skip_sid=client_id)
 
 
-@routes.post("/offer")
-async def offer(request):
-    params = await request.json()
-    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
-    if len(connected_clients) == 0:
-        peer_connection = RTCPeerConnection()
-        connected_clients.add(peer_connection)
-    else:
-        peer_connection = connected_clients.pop()
+@app.route('/rooms')
+def get_rooms():
+    return jsonify(rooms)
 
-    await peer_connection.setRemoteDescription(offer)
-
-    if not peer_connection.getTransceivers():
-        global video_track, audio_track
-        video_track = VideoStreamTrack()
-        audio_track = AudioStreamTrack()
-        peer_connection.addTrack(video_track)
-        peer_connection.addTrack(audio_track)
-
-    answer = await peer_connection.createAnswer()
-    await peer_connection.setLocalDescription(answer)
-    return web.json_response(
-        {"sdp": peer_connection.localDescription.sdp, "type": peer_connection.localDescription.type}
-    )
-
-
-async def cleanup(app):
-    for client in connected_clients:
-        await client.close()
-
-if __name__ == "__main__":
-    app = web.Application()
-    app.add_routes(routes)
-    app.on_shutdown.append(cleanup)
-    web.run_app(app)
-
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=1234, debug=True, allow_unsafe_werkzeug=True)
